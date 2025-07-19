8059bd63716ba23470f9232a4620e8062c3e9b86
John-Uko <ukojohn.j@gmail.com>
2025-07-19 21:58:17 +0100

    little python command

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 For all practices and assignments
 
 Done my first push successfully after several arrrgghhh
+
+Getting better with git and github <("-")>
--- see.py
+++ see.py
@@ -1,1 +1,9 @@
-print("Hello world")
+#print("Hello world")
+print("Welcome home! \nThis is where \"you\" can learn and do better. \n\"Good luck!\"")
+def name():
+    Username = input("Enter username")
+    Phone_number = input("phone number")
+    if Username == (4+4):
+        print("Welcome")
+    else: print("enter another username")
+print(name())
